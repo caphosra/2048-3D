a937fac0562f9a42fd822d651567f86fdc4f94e5
capra314cabra <capra314cabra@gmail.com>
2019-03-15 09:54:44 +0900

    First commit

---
+++ Assets/Scripts/Game/BlockManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject blockModel;
+
+    #region  Material Fields
+
+    [Header("Materials")]
+    [SerializeField]
+    private Material materialColor0;
+    [SerializeField]
+    private Material materialColor2;
+    [SerializeField]
+    private Material materialColor4;
+    [SerializeField]
+    private Material materialColor8;
+    [SerializeField]
+    private Material materialColor16;
+    [SerializeField]
+    private Material materialColor32;
+    [SerializeField]
+    private Material materialColor64;
+    [SerializeField]
+    private Material materialColor128;
+    [SerializeField]
+    private Material materialColor256;
+    [SerializeField]
+    private Material materialColorMore;
+
+    #endregion
+
+    private MeshRenderer meshRenderer;
+
+    private const int MAX_VALUE_OF_BLOCK = 256;
+    private Dictionary<int, Material> materialCatalog = new Dictionary<int, Material>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = blockModel.GetComponent<MeshRenderer>();
+        InitMaterials();
+    }
+
+    void InitMaterials()
+    {
+        materialCatalog.Add(0, materialColor0);
+        materialCatalog.Add(2, materialColor2);
+        materialCatalog.Add(4, materialColor4);
+        materialCatalog.Add(8, materialColor8);
+        materialCatalog.Add(16, materialColor16);
+        materialCatalog.Add(32, materialColor32);
+        materialCatalog.Add(64, materialColor64);
+        materialCatalog.Add(128, materialColor128);
+        materialCatalog.Add(256, materialColor256);
+        materialCatalog.Add(-1, materialColorMore);
+    }
+
+    public void ChangeMaterial(int val)
+    {
+        val = val > MAX_VALUE_OF_BLOCK ? -1 : val;
+        meshRenderer.material = materialCatalog[val];
+    }
+}
---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private Dictionary<(int x, int y), BlockManager> blockManagers;
+    private BlockBoard board = new BlockBoard();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blockManagers = new Dictionary<(int x, int y), BlockManager>();
+        for(int x = 1; x <= 4; x++)
+        {
+            for(int y = 1; y <= 4; y++)
+            {
+                var blockManager = GameObject.Find($"Block{x}-{y}").GetComponent<BlockManager>();
+                blockManagers.Add((x, y), blockManager);
+            }
+        }
+        board.RandomSpawn();
+        ChangeMaterialsAll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            board.Move(MoveDirection.UP, out List<(int, int)> changed);
+            if(changed.Count != 0 && !board.Full)
+            {
+                board.RandomSpawn();
+                ChangeMaterialsAll();
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            board.Move(MoveDirection.DOWN, out List<(int, int)> changed);
+            if(changed.Count != 0 && !board.Full)
+            {
+                board.RandomSpawn();
+                ChangeMaterialsAll();
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            board.Move(MoveDirection.LEFT, out List<(int, int)> changed);
+            if(changed.Count != 0 && !board.Full)
+            {
+                board.RandomSpawn();
+                ChangeMaterialsAll();
+            }
+        }
+        else if(Input.GetKeyDown(KeyCode.D))
+        {
+            board.Move(MoveDirection.RIGHT, out List<(int, int)> changed);
+            if(changed.Count != 0 && !board.Full)
+            {
+                board.RandomSpawn();
+                ChangeMaterialsAll();
+            }
+        }
+    }
+
+    void ChangeMaterialsAll()
+    {
+        for(int x = 1; x <= 4; x++)
+        {
+            for(int y = 1; y <= 4; y++)
+            {
+                blockManagers[(x, y)].ChangeMaterial(board[x, y]);
+            }
+        }
+    }
+}
